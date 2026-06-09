249c7be20725fb89fb8234570d0f60b6f34d595c
I578072 <l.priya@sap.com>
2026-06-09 23:34:08 +0530

    added employee

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,1 +1,18 @@
+public class Employee {
 
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
